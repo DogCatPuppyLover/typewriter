50e38a228fb4f2b955f2846a2cb5be49f9f5806d
Isla <79656307+DogCatPuppyLover@users.noreply.github.com>
2022-01-13 19:31:39 -0800

    Prevented attributes from being saved + p >> div + rearranged code

--- scripts.js
+++ scripts.js
@@ -1,41 +1,35 @@
-//https://www.soundjay.com/typewriter-sounds.html
-var keystroke = new Audio("assets/audio/typewriter keystroke.mp3");
+// VARIABLE DEFINITIONS
+var keystroke = new Audio("assets/audio/typewriter keystroke.mp3"); // Typewriter key sound effects: https://www.soundjay.com/typewriter-sounds.html
 var keystroke2 = new Audio("assets/audio/typewriter keystroke.mp3");
 var space = new Audio("assets/audio/typewriter space.mp3");
 var linebreak = new Audio("assets/audio/typewriter linebreak.mp3");
 var backspace = new Audio("assets/audio/typewriter backspace.mp3");
 var paperIn = new Audio("assets/audio/typewriter paper in.mp3");
 var paperOut = new Audio("assets/audio/typewriter paper out.mp3");
-keystroke.volume = 0.25;
-keystroke2.volume = 0.25;
-var paragraphTags = ["div", "p", "ul", "ol", "h1", "h2", "h3", "h4", "h5", "h6"]
 
+var paragraphTags = ["div", "p", "ul", "ol", "h1", "h2", "h3", "h4", "h5", "h6"]
 var file = 0;
 var moduloKey = 0;
-
 var turndownService = new TurndownService();
+var editor = document.getElementById("editor");
+var toolbox = document.getElementById("toolbox");
 
+// SOUND EFFECT SETUP
+keystroke.volume = 0.25;
+keystroke2.volume = 0.25;
+
+// CONTENTEDITABLE SETUP
 document.execCommand("defaultParagraphSeparator", false, "div");
 document.execCommand("insertBrOnReturn", false, false);
 document.execCommand("useCSS", false, true);
 
-var editor = document.getElementById("editor");
-
+// FUNCTION DEFINITIONS
 var switchTab = function (i) {
   editor.innerHTML = localStorage.getItem("file_" + i);
   document.getElementById("file_" + file).classList.remove("activeTab");
   document.getElementById("file_" + i).classList.add("activeTab");
   file = i;
   clearEditClasses(document);
-}
-
-var addToolboxTab = function (i) {
-  tab = document.createElement("button");
-  tab.innerHTML = "File " + (i + 1);
-  tab.id = "file_" + i;
-  tab.className = "tab";
-  tab.onclick = function(){switchTab(i);};
-  document.getElementById("tabs").appendChild(tab);
 }
 
 var appendStyles = function (styles) {
@@ -51,65 +45,37 @@ var clearEditClasses = function (node) {
   let elems = node.querySelectorAll(".editableFocus");
   [].forEach.call(elems, function(el) {
     el.classList.remove("editableFocus");
+    if (el.classList.length == 0) {
+      el.removeAttribute("class");
+    }
   });
 }
 
-if (localStorage.getItem("file_0") !== null) {
-  i = 0;
-  var tab;
-  while (localStorage.getItem("file_" + i) !== null) {
-    addToolboxTab(i);
-    i++;
-  }
-  switchTab(0);
-} else {
-  localStorage.setItem("file_0", editor.innerHTML);
-  addToolboxTab(0);
-  switchTab(0);
-}
-
-if (localStorage.getItem("typewriterSounds") == "true") {
-  paperIn.play();
-}
-
-if (localStorage.getItem("spellcheck") !== null) {
-  editor.setAttribute("spellcheck", localStorage.getItem("spellcheck"));
-}
-
-if (localStorage.getItem("userStyles") !== null) {
-  appendStyles(localStorage.getItem("userStyles"));
-}
-
-if (localStorage.getItem("focusMode") == "true") {
-  editor.classList.add("focusMode");
-
-  // https://stackoverflow.com/a/29979356
-  let selectionContainer = null;
-  function updateSelectionContainer() {
-    let newSelectionContainer = null;
-    let sel;
-    if (window.getSelection && (sel = window.getSelection()).rangeCount) {
-      newSelectionContainer = sel.getRangeAt(0).commonAncestorContainer;
-      while (newSelectionContainer.nodeType != 1 || !paragraphTags.includes(newSelectionContainer.tagName.toLowerCase())) {
-        newSelectionContainer = newSelectionContainer.parentNode;
-      }
+var clearAttributes = function (node) {
+  // https://stackoverflow.com/a/22270709
+  let elems = node.getElementsByTagName("*");
+  [].forEach.call(elems, function(el) {
+    // https://stackoverflow.com/a/27664638
+    while (el.attributes.length > 0) {
+      el.removeAttribute(el.attributes[0].name);
     }
-    if (newSelectionContainer != selectionContainer) {
-      clearEditClasses(document);
-      if (newSelectionContainer) {
-        newSelectionContainer.classList.add("editableFocus");
-      }
-      selectionContainer = newSelectionContainer;
-    }
-  }
+  });
+}
 
-  if ("onselectionchange" in document) {
-    document.onselectionchange = updateSelectionContainer;
-  } else {
-    var el = document.getElementById("editor");
-    el.onmousedown = el.onmouseup = el.onkeydown = el.onkeyup = el.oninput = updateSelectionContainer;
-    window.setInterval(updateSelectionContainer, 100);
-  }
+var addToolboxTab = function (i) {
+  tab = document.createElement("button");
+  tab.innerHTML = "File " + (i + 1);
+  tab.id = "file_" + i;
+  tab.className = "tab";
+  tab.onclick = function(){switchTab(i);};
+  document.getElementById("tabs").appendChild(tab);
+}
+
+var save = function () {
+  let temp = document.createElement("div");
+  temp.innerHTML = editor.innerHTML;
+  clearAttributes(temp);
+  localStorage.setItem("file_" + file, temp.innerHTML);
 }
 
 var saveFile = function (filename, data, type) {
@@ -139,7 +105,7 @@ var openFile = function () {
     } else if (fileItem.type === "text/plain") {
       var splitByParagraph = "";
       for (let i = 0; i < e.target.result.split(/\n|\n\r|\r/).length; i++) {
-        splitByParagraph = splitByParagraph + "<p>" + e.target.result.split(/\n|\n\r|\r/)[i] + "</p>";
+        splitByParagraph = splitByParagraph + "<div>" + e.target.result.split(/\n|\n\r|\r/)[i] + "</div>";
       }
       editor.innerHTML = splitByParagraph;
     } else {
@@ -152,14 +118,7 @@ var openFile = function () {
   };
 }
 
-var save = function () {
-  let temp = document.createElement("div");
-  temp.innerHTML = editor.innerHTML;
-  clearEditClasses(temp);
-  localStorage.setItem("file" + file, editor.innerHTML);
-}
-
-var newTab = function () {
+var newFile = function () {
   var i = 0;
   while (localStorage.getItem("file_" + i) !== null) {i++;}
   localStorage.setItem("file_" + i, "It was a dark and stormy night . . .");
@@ -167,6 +126,39 @@ var newTab = function () {
   switchTab(i);
 }
 
+// PREFERENCES
+
+if (localStorage.getItem("file_0") !== null) {
+  i = 0;
+  var tab;
+  while (localStorage.getItem("file_" + i) !== null) {
+    addToolboxTab(i);
+    i++;
+  }
+  switchTab(0);
+} else {
+  localStorage.setItem("file_0", editor.innerHTML);
+  addToolboxTab(0);
+  switchTab(0);
+}
+
+if (localStorage.getItem("typewriterSounds") == "true") {
+  paperIn.play();
+}
+
+if (localStorage.getItem("spellcheck") !== null) {
+  editor.setAttribute("spellcheck", localStorage.getItem("spellcheck"));
+}
+
+if (localStorage.getItem("userStyles") !== null) {
+  appendStyles(localStorage.getItem("userStyles"));
+}
+
+if (localStorage.getItem("focusMode") == "true") {
+  editor.classList.add("focusMode");
+}
+
+// EVENTLISTENERS
 editor.addEventListener("input", function (event) {
   if (localStorage.getItem("typewriterSounds") == "true") {
     if (event.keyCode == 13) {
@@ -201,8 +193,7 @@ document.body.addEventListener("mousemove", function () {
   document.body.classList.remove("hide-scrollbars"); //Show scrollbars
 }, false);
 
-//Keypress detection (for commands and sound effects)
-
+// Keypress detection (for commands and sound effects)
 document.addEventListener("keydown", function (event) {
   if (window.navigator.platform.match("Mac") ? event.metaKey : event.ctrlKey) {
     if (event.keyCode === 83) {
@@ -231,7 +222,7 @@ document.addEventListener("keydown", function (event) {
       document.execCommand("italic", false, null);
     } else if (event.keyCode == 48) {
       event.preventDefault();
-      document.execCommand("formatBlock", false, "<p>");
+      document.execCommand("formatBlock", false, "<div>");
     } else if (event.keyCode == 49) {
       event.preventDefault();
       document.execCommand("formatBlock", false, "<h1>");
@@ -303,3 +294,25 @@ document.addEventListener("keydown", function (event) {
     }
   }
 }, false);
+
+// Add a class to focused elements, for detecting focus on contenteditable elements: https://stackoverflow.com/a/29979356
+let selectionContainer = null;
+function updateSelectionContainer() {
+  let newSelectionContainer = null;
+  let sel;
+  if (window.getSelection && (sel = window.getSelection()).rangeCount) {
+    newSelectionContainer = sel.getRangeAt(0).commonAncestorContainer;
+    while (newSelectionContainer.nodeType != 1 || !paragraphTags.includes(newSelectionContainer.tagName.toLowerCase())) {
+      newSelectionContainer = newSelectionContainer.parentNode;
+    }
+  }
+  if (newSelectionContainer != selectionContainer) {
+    clearEditClasses(document);
+    if (newSelectionContainer) {
+      newSelectionContainer.classList.add("editableFocus");
+    }
+    selectionContainer = newSelectionContainer;
+  }
+}
+
+document.addEventListener('selectionchange', updateSelectionContainer); // Trigger focus detection
